69d779333797e2f02efce9f73a3de87d8182ad63
Roman Volkov <riv10d@pm.me>
2025-08-23 10:30:57 +0200

    Initial version

---
+++ webrip19.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+set -e
+
+# See README.md for help.
+
+#
+# SPDX-License-Identifier: WTFPL
+#
+# Authors and copyright holders provide the licensed software “as is” and do not
+# provide any warranties, including the merchantability of the software and
+# suitability for any purpose.
+#
+
+halt() {
+    echo "$1"
+    exit 1
+}
+
+# --- SETTINGS BEGIN
+PLAYLIST="$PWD/playlist.m3u"
+TMPDIR=/tmp/WebRip19
+YTDLP_ARGS=( --extractor-args "youtube:player-client=tv_embedded" \
+    --cookies "$PWD/www.youtube.com_cookies.txt" \
+    --abort-on-unavailable-fragments \
+    --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0" \
+    -S vcodec:h264 )
+MPLAYER_VIDEO_ARGS=( -vf scale=1280:720 )
+SVTENC_ARGS=( --crf 38 --enable-variance-boost 1 --tune 2 --preset 0 )
+OPUSENC_ARGS=( --bitrate 64 )
+MKVPROPEDIT_ARGS=( --edit track:a1 --set language=ukr )
+BYPASS_VAPOURSYNTH=1
+
+VAPOUR_SYNTH_TPL='
+import vapoursynth as vs
+import havsfunc as haf
+
+core = vs.core
+clip = core.ffms2.Source(source='\''%%INPUT_FILE%%'\'')
+clip = haf.QTGMC(clip, Preset='\''Slower'\'', FPSDivisor=2, TFF=False)
+clip = core.resize.Spline36(clip, 1280, 720)
+clip.set_output()
+'
+
+ENCODER_TAG='
+<Tags>
+  <Tag>
+    <Simple>
+      <Name>ENCODER</Name>
+      <String>%%ENCODER_VERSION%%</String>
+    </Simple>
+    <Simple>
+      <Name>ENCODER_OPTIONS</Name>
+      <String>%%ENCODER_OPTIONS%%</String>
+    </Simple>
+  </Tag>
+</Tags>
+'
+# --- SETTINGS END
+
+# --- PREREQUISITES BEGIN
+
+which wget >/dev/null 2>&1 || halt "Please install wget"
+which yt-dlp >/dev/null 2>&1 || halt "Please install yt-dlp"
+which avifenc >/dev/null 2>&1 || halt "Please install libavif"
+which SvtAv1EncApp >/dev/null 2>&1 || halt "Please install svt-av1"
+which opusenc >/dev/null 2>&1 || halt "Please install opus-tools"
+which mkvmerge >/dev/null 2>&1 || halt "Please install mkvtoolnix"
+which mkvpropedit >/dev/null 2>&1 || halt "Please install mkvtoolnix"
+which mplayer >/dev/null 2>&1 || halt "Please install mplayer"
+which magick >/dev/null 2>&1 || halt "Please install imagemagick"
+if [[ "$BYPASS_VAPOURSYNTH" != 1 ]]
+then
+    which vspipe >/dev/null 2>&1 || halt "Please install vapoursynth"
+fi
+
+# --- PREREQUISITES END
+
+if [[ -d "$TMPDIR" ]]
+then
+    rm -rf "$TMPDIR"
+fi
+
+CURR_DIR="$PWD"
+OUT_DIR="$HOME/Videos/WebRip19"
+mkdir "$TMPDIR"
+mkdir -p "$OUT_DIR"
+
+cd "$TMPDIR"
+
+line=$(head -n 1 "$PLAYLIST")
+if [[ "$line" != "#EXTM3U" ]]
+then
+    halt "Missing #EXTM3U file header in playlist"
+fi
+
+NUMBER=0
+
+while IFS="" read -r line || [ -n "$line" ]
+do
+    # Download the video file, converting to Matroska
+    if [[ "$line" == https://* || "$line" == http://* ]]
+    then
+        yt-dlp --write-thumbnail --abort-on-unavailable-fragments \
+            -t mkv "${YTDLP_ARGS[@]}" "$line"
+    elif [[ "$line" == file://* ]]
+    then
+        yt-dlp --enable-file-urls --write-thumbnail \
+            -o "%(title)s.%(ext)s" \
+            -t mkv "${YTDLP_ARGS[@]}" "$line"
+    else
+        continue
+    fi
+
+    mkvfiles=( *.mkv )
+    webpfiles=( *.webp )
+
+    # Make a VapourSynth script
+    echo "$VAPOUR_SYNTH_TPL" | sed "s/%%INPUT_FILE%%/${mkvfiles[0]}/" > tmp.vpy
+
+    # Make a XML tags file with encoder options for the video track
+    svtav1_ver="$(SvtAv1EncApp --version)"
+    svtav1_args="${SVTENC_ARGS[@]}"
+    echo "$ENCODER_TAG" \
+        | sed "s/%%ENCODER_VERSION%%/$svtav1_ver/" \
+        | sed "s/%%ENCODER_OPTIONS%%/$svtav1_args/" > video_tag.xml
+
+    # Process video
+    if [[ $BYPASS_VAPOURSYNTH == "1" ]]
+    then
+        mplayer "${mkvfiles[0]}" -noconsolecontrols -really-quiet \
+            "${MPLAYER_VIDEO_ARGS[@]}" -vo yuv4mpeg:file=/dev/stdout -ao null | \
+            SvtAv1EncApp "${SVTENC_ARGS[@]}" -b tmp.ivf -i stdin
+    else
+        vspipe -c y4m tmp.vpy - | \
+            SvtAv1EncApp "${SVTENC_ARGS[@]}" -b tmp.ivf -i stdin
+    fi
+
+    # Process audio
+    mplayer "${mkvfiles[0]}" -noconsolecontrols -really-quiet -vo null \
+        -ao pcm:fast:file=/dev/stdout | \
+        opusenc "${OPUSENC_ARGS[@]}" --ignorelength - tmp.opus
+
+    # Merge Matroska
+    mkvmerge -o tmp.mkv tmp.ivf tmp.opus
+
+    # Update tags and optionally attach the thumbnail
+    if [[ -f "${webpfiles[0]}" ]]
+    then
+        magick "${webpfiles[0]}" cover.png
+        avifenc -q 51 -c svt -s 0 -j 4 -d 8 -y 420 -a avif=1 -a tune=0 cover.png cover.avif
+        MKVPROPEDIT_ARGS+=( --add-attachment cover.avif )
+    fi
+    mkvpropedit tmp.mkv --tags track:v1:video_tag.xml --add-track-statistics-tags \
+        "${MKVPROPEDIT_ARGS[@]}"
+
+    # Move resulting file and cleanup
+    prefix=$(printf "%03d\n" $NUMBER)
+    mv tmp.mkv "$OUT_DIR/$prefix# ${mkvfiles[0]}"
+    rm -f *.mkv *.vpy *.xml *.ivf *.opus *.jpg *.avif *.ffindex
+
+    NUMBER=$((NUMBER + 1))
+done < "$PLAYLIST"
